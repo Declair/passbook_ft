0626d68a516049be8771a72acad6886ee04c8e66
Jihao DENG <jihaod@student.unimelb.edu.au>
2021-06-23 16:56:26 +1000

    improve the list view

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -4,26 +4,45 @@ const Accounts = ({ accounts }) => {
   return (
       <div>
         {accounts.map((account) => {
+          console.log(account)
           return <Account key={account.aid} {...account} />;
         })}
       </div>
   );
 };
 
-const Account = ({ aid, name, password, continueWith, properties }) => {
+const Account = ({ aid, name, password, continueWithAid, continueWithName, properties }) => {
   return (
     <div className='card'>
       <div className='card-header'>
         {name}
       </div>
-      <div className='card-container'>
-        <p>password: {password}</p>
-        <p>continueWith: {continueWith}</p>
+      <div className='card-container' id={`${'account-'+aid}`}>
+        <tr>
+          <td>Account Name</td>
+          <td>{name}</td>
+        </tr>
+        {  // if it's continued with another account, then show a link to anchor of that account
+          continueWithAid &&
+          (<tr>
+            <td>continueWith</td>
+            <td><a href={`${'#account-'+continueWithAid}`}>{continueWithName}</a></td>
+          </tr>)
+        }
+        {  // if it doesn't have continueWithAid, then it should have a password
+          !continueWithAid &&
+          (<tr>
+            <td>Password</td>
+            <td>{password}</td>
+          </tr>)
+        }
         {properties.map((property) => {
           return (
-            <div key={property.pid}>
-              {property.name} : {property.value}
-            </div>
+            <tr key={property.pid} 
+              className={`${property.fixed ? 'fixed-property':''}`}>
+              <td>{property.name}</td>
+              <td>{property.value}</td>
+            </tr>
           )
         })}
       </div>
