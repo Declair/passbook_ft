46a19a48933ea9fc6ebea9471431c82c26c4bea6
Jihao DENG <jihaod@student.unimelb.edu.au>
2021-06-26 15:50:20 +1000

    removing warnings in list_view

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -17,33 +17,35 @@ const Account = ({ aid, name, password, continueWithAid, continueWithName, prope
         {name}
       </div>
       <div className='card-container' id={`${'account-'+aid}`}>
-        <tr>
-          <td>Account Name</td>
-          <td>{name}</td>
-        </tr>
-        {  // if it's continued with another account, then show a link to anchor of that account
-          continueWithAid &&
-          (<tr>
-            <td>continueWith</td>
-            <td><a href={`${'#account-'+continueWithAid}`}>{continueWithName}</a></td>
-          </tr>)
-        }
-        {  // if it doesn't have continueWithAid, then it should have a password
-          !continueWithAid &&
-          (<tr>
-            <td>Password</td>
-            <td>{password}</td>
-          </tr>)
-        }
-        {properties.map((property) => {
-          return (
-            <tr key={property.pid} 
-              className={`${property.fixed ? 'fixed-property':''}`}>
-              <td>{property.name}</td>
-              <td>{property.value}</td>
-            </tr>
-          )
-        })}
+        <table><tbody>
+          <tr>
+            <td>Account Name</td>
+            <td>{name}</td>
+          </tr>
+          {  // if it's continued with another account, then show a link to anchor of that account
+            continueWithAid &&
+            (<tr>
+              <td>continueWith</td>
+              <td><a href={`${'#account-'+continueWithAid}`}>{continueWithName}</a></td>
+            </tr>)
+          }
+          {  // if it doesn't have continueWithAid, then it should have a password
+            !continueWithAid &&
+            (<tr>
+              <td>Password</td>
+              <td>{password}</td>
+            </tr>)
+          }
+          {properties.map((property) => {
+            return (
+              <tr key={property.pid} 
+                className={`${property.fixed ? 'fixed-property':''}`}>
+                <td>{property.name}</td>
+                <td>{property.value}</td>
+              </tr>
+            )
+          })}
+        </tbody></table>
       </div>
     </div>
   )
